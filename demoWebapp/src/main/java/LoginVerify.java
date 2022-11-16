2629f099965e0e973710fb3e830b86a381b3dd3c
vassilikan <vassilikan@DESKTOP-SUT6VEF>
2022-11-16 11:35:41 +0100

    init commit

---
+++ demoWebapp/src/main/java/LoginVerify.java
@@ -0,0 +1,49 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.annotation.WebServlet;
+
+/**
+ * Servlet implementation class LoginVerify
+ */
+//@WebServlet("/LoginVerify")
+public class LoginVerify extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginVerify() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = request.getParameter("uname");
+		String pwd = request.getParameter("password");
+		if(name.equals("admin") && pwd.equals("admin")) {
+			System.out.println("OK");
+		} else {
+			System.out.println("KO");
+		}
+
+	}
+
+}
